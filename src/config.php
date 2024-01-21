6698fb74873f5eebaa1e84582f2fecc411146026
Jadon Duby <jadon.duby@neofinancial.com>
2024-01-20 22:03:42 -0700

    adds config

---
+++ src/config.php
@@ -0,0 +1,8 @@
+<?php
+
+   define('HOST', getenv('HOST') );
+   define('DB_USERNAME', getenv('DB_USERNAME') );
+   define('DB_PASSWORD', getenv('DB_PASSWORD') );
+   define('DB_DATABASE', getenv('DB_DATABASE') );
+
+?>
