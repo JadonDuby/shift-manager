a5d81fc27a9f91989c021cdac14a36eaa30efb20
Jadon Duby <jadon.duby@neofinancial.com>
2024-02-02 18:04:53 -0700

    removes unused links

--- src/welcomeAdmin.php
+++ src/welcomeAdmin.php
@@ -4,8 +4,9 @@
    </head>
    <body>
       <h1>Welcome <?php if (isset($_COOKIE["user"])) echo $_COOKIE["user"] . "!"; ?></h1>
-	  <h3><a class="manageemployee" href="viewEmp.php">Manage Employee</a></h3>
-      <h3><a class="manageshifts" href="viewShifts.php">Manage Shifts</a></h3>
+	  <!-- <h3><a class="manageemployee" href="viewEmp.php">Manage Employee</a></h3> -->
+      <!-- <h3><a class="manageshifts" href="viewShifts.php">Manage Shifts</a></h3> -->
+      <h3><a class = "shifts" href = "shift_ui_admin.php">Edit Shifts</a></h3>
       <h3><a href="index.php">Sign Out</a></h3>
    </body>
 </html>
